844e57477993758211c941a4e5a5e9eb069f794b
Wilbert Sequeira <wilbertcr@gmail.com>
2016-01-23 16:54:31 -0800

    Changes to VM

--- vagrant/forum/forum.py
+++ vagrant/forum/forum.py
@@ -43,7 +43,8 @@ POST = '''\
     <div class=post><em class=date>%(time)s</em><br>%(content)s</div>
 '''
 
-## Request handler for main page
+
+# Request handler for main page
 def View(env, resp):
     '''View is the 'main page' of the forum.
 
@@ -56,13 +57,14 @@ def View(env, resp):
     resp('200 OK', headers)
     return [HTML_WRAP % ''.join(POST % p for p in posts)]
 
-## Request handler for posting - inserts to database
+
+# Request handler for posting - inserts to database
 def Post(env, resp):
-    '''Post handles a submission of the forum's form.
-  
+    """Post handles a submission of the forum's form.
+
     The message the user posted is saved in the database, then it sends a 302
     Redirect back to the main page so the user can see their new post.
-    '''
+    """
     # Get post content
     input = env['wsgi.input']
     length = int(env.get('CONTENT_LENGTH', 0))
@@ -79,29 +81,30 @@ def Post(env, resp):
     # 302 redirect back to the main page
     headers = [('Location', '/'),
                ('Content-type', 'text/plain')]
-    resp('302 REDIRECT', headers) 
+    resp('302 REDIRECT', headers)
     return ['Redirecting']
 
-## Dispatch table - maps URL prefixes to request handlers
+
+# Dispatch table - maps URL prefixes to request handlers
 DISPATCH = {'': View,
             'post': Post,
-	    }
+            }
 
-## Dispatcher forwards requests according to the DISPATCH table.
+
+# Dispatcher forwards requests according to the DISPATCH table.
 def Dispatcher(env, resp):
-    '''Send requests to handlers based on the first path component.'''
+    """Send requests to handlers based on the first path component."""
     page = util.shift_path_info(env)
     if page in DISPATCH:
         return DISPATCH[page](env, resp)
     else:
         status = '404 Not Found'
         headers = [('Content-type', 'text/plain')]
-        resp(status, headers)    
+        resp(status, headers)
         return ['Not Found: ' + page]
 
 
 # Run this bad server only on localhost!
 httpd = make_server('', 8000, Dispatcher)
-print "Serving HTTP on port 8000..."
+print("Serving HTTP on port 8000...")
 httpd.serve_forever()
-
--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,29 +3,60 @@
 # 
 
 import time
+import psycopg2
+import bleach
 
-## Database connection
-DB = []
+# Database connection
+def connect():
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=forum")
 
-## Get posts from database.
+
+# Get posts from database.
+
+
 def GetAllPosts():
-    '''Get all the posts from the database, sorted with the newest first.
-
+    """Get all the posts from the database, sorted with the newest first.
     Returns:
       A list of dictionaries, where each dictionary has a 'content' key
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
-    '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    """
+    # Connect to database
+    db_connection = connect()
+    # Get db cursor
+    db_cursor = db_connection.cursor()
+    # Execute query
+    db_cursor.execute("SELECT * FROM posts ORDER BY time DESC")
+    # Fetch results
+    db_result = db_cursor.fetchall()
+    # Process results(turn them into a dictionary)
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in db_result]
+    # Close cursor and connection to db.
+    db_cursor.close()
+    db_connection.close()
+    # Return posts
     return posts
 
-## Add a post to the database.
+# Add a post to the database.
+
+
 def AddPost(content):
-    '''Add a new post to the database.
+    """Add a new post to the database.
 
     Args:
       content: The text content of the new post.
-    '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    """
+    # Clean user input from code.
+    content = bleach.clean(content)
+    # Connect to the database
+    db_connection = connect()
+    # Get db cursor
+    db_cursor = db_connection.cursor()
+    # Insert post into db
+    db_cursor.execute("INSERT INTO posts (content) VALUES (%(str)s);", {'str': content})
+    # Commit post insertion if all went well.
+    db_connection.commit()
+    # Close cursor and connection to db.
+    db_cursor.close()
+    db_connection.close()
