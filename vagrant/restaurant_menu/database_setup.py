1641bf4bd2eb1685df9d76bba3b82a76bf8b9641
Wilbert Sequeira <wilbertcr@gmail.com>
2016-02-03 13:41:53 -0800

    Adding restaurant_menu

---
+++ vagrant/restaurant_menu/database_setup.py
@@ -0,0 +1,29 @@
+import sys
+from sqlalchemy import \
+    Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+
+engine = create_engine('postgresql://vagrant:vagrantvm@localhost:5432/restaurant')
+Base.metadata.create_all(engine)
