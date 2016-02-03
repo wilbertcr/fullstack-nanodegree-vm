160402bd2db9128aa2f9a75a83148b77957285d8
Wilbert Sequeira <wilbertcr@gmail.com>
2016-02-03 13:48:09 -0800

    Changed format a little bit.

--- vagrant/shelter/puppies.py
+++ vagrant/shelter/puppies.py
@@ -7,8 +7,8 @@ Base = declarative_base()
 
 class Shelter(Base):
     __tablename__ = 'shelter'
-    id = Column(Integer, primary_key = True)
-    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
     address = Column(String(250))
     city = Column(String(80))
     state = Column(String(20))
@@ -19,7 +19,7 @@ class Puppy(Base):
     __tablename__ = 'puppy'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    gender = Column(String(6), nullable = False)
+    gender = Column(String(6), nullable=False)
     dateOfBirth = Column(Date)
     picture = Column(String)
     shelter_id = Column(Integer, ForeignKey('shelter.id'))
@@ -28,6 +28,4 @@ class Puppy(Base):
 
 
 engine = create_engine('sqlite:///puppyshelter.db')
- 
-
 Base.metadata.create_all(engine)
