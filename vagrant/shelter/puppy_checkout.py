00da7dbbb959ef89767ab523e4e78b2b8ca76392
Wilbert Sequeira <wilbertcr@gmail.com>
2016-02-08 19:38:28 -0800

    Exercise 5: Started exercise 5. Managed to get a successful response from Google Maps.

--- vagrant/shelter/puppy_checkout.py
+++ vagrant/shelter/puppy_checkout.py
@@ -1,28 +1,32 @@
 #This is how a file making use of the objects(rather than creating them in the database)
 #would look like.
 
+import json
 import datetime
+import urllib.parse, urllib.request
+import time
 from sys import maxsize
 from sqlalchemy import create_engine, func, Column, Integer, Table, MetaData, asc
 from sqlalchemy.orm import sessionmaker
 from migrate import changeset
 from puppies import Base, Shelter, Puppy
-5
+
 engine = create_engine('sqlite:///puppyshelter.db', echo=True)
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 """:type: sqlalchemy.orm.Session"""
 session = DBSession()
 
-f = open('../../../puppies_key.txt', 'r')
-api_key = f.readline()
-url_base = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+def get_key():
+    f = open('../../../puppies_key.txt', 'r')
+    api_key = f.readline()
+    return api_key
 
 
 def get_shelter_occupancy():
     #Gets table of the form:
     # |shelter_id|puppies_in_shelter|
-    shelter_occupancy = session.query(Shelter.id, func.count(Shelter.id).label('cnt')).\
+    shelter_occupancy = session.query(Shelter, func.count(Shelter.id).label('cnt')).\
         join(Puppy, Shelter.id == Puppy.shelter_id).\
         group_by(Shelter.name).\
         order_by(asc('cnt')).\
@@ -30,20 +34,17 @@ def get_shelter_occupancy():
     return shelter_occupancy
 
 
-def refresh_occupancy(shelter_occupancy):
+def update_occupancy(shelter_occupancy):
     #We use the above to refresh the current
     #occupancy.
     for cur_count in shelter_occupancy:
         shelter = session.query(Shelter).\
-            filter_by(id=cur_count[0]).one()
+            filter_by(id=cur_count[0].id).one()
         shelter.cur_occupancy = cur_count[1]
         session.commit()
 
-shelter_count = get_shelter_occupancy()
-
-min_occupancy = maxsize
-shelter_id = 0
-print(shelter_count)
+shelter_occupancy = get_shelter_occupancy()
+update_occupancy(shelter_occupancy)
 
 
 def add_new_puppy(name, gender, dateOfBirth, picture, shelter_id, weight):
@@ -62,5 +63,28 @@ def add_new_puppy(name, gender, dateOfBirth, picture, shelter_id, weight):
     session.add(new_puppy)
     session.commit()
     occupancy = get_shelter_occupancy()
-    refresh_occupancy(occupancy)
+    update_occupancy(occupancy)
     return new_puppy
+
+def get_distances(origin_address):
+    api_key = get_key()
+    url_base = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+    shelter_occupancy = get_shelter_occupancy()
+    destinations_list = []
+    for row in shelter_occupancy:
+        shelter = row[0]
+        address = shelter.address+', '+shelter.city+', '+shelter.state+' '+shelter.zipCode
+        destinations_list.append(address)
+    destinations = "|".join(destinations_list)
+    query_args = {'origins': origin_address,
+                  'destinations': destinations,
+                  'key': api_key}
+    encoded_args = urllib.parse.urlencode(query_args)
+    url = url_base+encoded_args
+    response = urllib.request.urlopen(url)
+    print(response.read().decode('utf-8'))
+
+
+origin_address = '655 12th Street, Oakland, CA 94607, USA'
+get_distances(origin_address)
+
