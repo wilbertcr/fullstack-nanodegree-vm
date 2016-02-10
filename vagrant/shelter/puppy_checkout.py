16de595be9182b6cbea0a8fc1fdf6cb7296df197
Wilbert Sequeira <wilbertcr@gmail.com>
2016-02-10 13:43:42 -0800

    Exercise 5: Completed exercise using Google Maps Distance Matrix API. I used it to get driving distances to shelters from the user's location. This way, the user gets the closest shelter with space availability.

--- vagrant/shelter/puppy_checkout.py
+++ vagrant/shelter/puppy_checkout.py
@@ -1,5 +1,3 @@
-#This is how a file making use of the objects(rather than creating them in the database)
-#would look like.
 
 import json
 import datetime
@@ -38,10 +36,12 @@ def get_shelter_occupancy():
 def update_occupancy(shelter_occupancy):
     #We use the above to refresh the current
     #occupancy.
+    MAX_DOGS = 45
     for cur_count in shelter_occupancy:
         shelter = session.query(Shelter).\
             filter_by(id=cur_count[0].id).one()
         shelter.cur_occupancy = cur_count[1]
+        shelter.maximum_capacity = MAX_DOGS
         session.commit()
 
 shelter_occupancy = get_shelter_occupancy()
@@ -102,42 +102,63 @@ def get_distances(origin_address):
     shelter_occupancy = get_shelter_occupancy()
     shelters = []
     destinations_list = []
+    #Here we populate a list(destinations_list) containing the shelters' addresses
+    #in the format that google distance matrix API wants them.
     for row in shelter_occupancy:
         shelter = row[0]
         shelters.append(shelter)
         address = shelter.address+', '+shelter.city+', '+shelter.state+' '+shelter.zipCode
         destinations_list.append(address)
+    #Google distance matrix wants addresses to be separated by "|" characters.
+    #Let's make google happy.
     destinations = "|".join(destinations_list)
+    #Let's put togethere the query arguments.
     query_args = {'origins': origin_address,
                   'destinations': destinations,
                   'key': api_key}
+    #This is very transparent.
     encoded_args = urllib.parse.urlencode(query_args)
     url = url_base+encoded_args
+    #Now we send the query
     response = urllib.request.urlopen(url)
+    #Then we read and parse the response.
     parsed_json = json.loads(response.read().decode('utf-8'))
+    #Check this out for info on the json structure:
+    #https://developers.google.com/maps/documentation/distance-matrix/
+
     rows = parsed_json['rows']
     driving_distances = rows[0]['elements']
     i = 0
     container = []
+    #Now we're gonna put together shelter objects and their
+    #driving distances so we can choose based on how
+    #far they are.
     for driving_distance in driving_distances:
         container.append({'shelter_object': shelters[i],
                           'address': destinations_list[i],
                           'driving_distance': driving_distance})
         shelter = shelters[i]
-        print(shelter.id)
+        #print(shelter.id)
         address = destinations_list[i]
-        print(destinations_list[i])
+        #print(destinations_list[i])
         value = driving_distance['duration']['value']
-        print(value)
+        #print(value)
         i += 1
 
+    #I modified this to sort this objects.
     merge_sort(container)
+    #Now the "shelters" are ordered from the closest to
+    #farther away.
+    #I am returning the closest shelter that has
+    #space available, but we could perfectly return
+    #the complete list and have the user choose.
+    #Many choices here, all depending on the use case.
     for shelter in container:
-        print(shelter['address'])
-        print(shelter['driving_distance'])
-
+        if(shelter['shelter_object'].cur_occupancy < shelter['shelter_object'].maximum_capacity):
+            print(shelter['address'])
+            print(shelter['driving_distance'])
+            return shelter
 
 
 origin_address = '655 12th Street, Oakland, CA 94607, USA'
 get_distances(origin_address)
-
