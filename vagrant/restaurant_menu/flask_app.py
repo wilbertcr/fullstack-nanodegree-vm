0af40fe89f49a58b9fe8ea6aed7c30cdd0a767f7
Wilbert Sequeira <wilbertcr@gmail.com>
2016-03-01 08:44:28 -0800

    Full Stack Foundations: Working on Lesson 3

--- vagrant/restaurant_menu/flask_app.py
+++ vagrant/restaurant_menu/flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
 from flask import request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -17,40 +17,24 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    output = ''
-    for i in items:
-        output += i.name
-        output += '</br>'
-        output += i.price
-        output += '</br>'
-        output += i.description
-        output += '</br>'
-        output += '</br>'
-    return output
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
 # Task 1: Create route for newMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/menu/new')
+@app.route('/restaurants/<int:restaurant_id>/menu/new',methods=['GET','POST'])
 def newMenuItem(restaurant_id):
-    if request.method == 'GET':
-        output = ""
-        output += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/{0}/menu/add_new'>'''.format(restaurant_id)
-        output += '''<h2>New menu item</h2>'''
-        output += '''Name: <input name="name" type="text"><br>'''
-        output += '''Description: <input name="description" type="text"><br>'''
-        output += '''Price: <input name="price" type="text"><br>'''
-        output += '''Course:<input name="course" type="text"><br>'''
-        output += '''<input type="submit" value="Submit"> </form>'''
-        return output
-
-
-@app.route('/restaurants/<int:restaurant_id>/menu/add_new', methods=['POST'])
-def recordMenuItem(restaurant_id):
     try:
+        if request.method == 'GET':
+            output = ""
+            output += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/{0}/menu/new'>'''.format(restaurant_id)
+            output += '''<h2>New menu item</h2>'''
+            output += '''Name: <input name="name" type="text"><br>'''
+            output += '''Description: <input name="description" type="text"><br>'''
+            output += '''Price: <input name="price" type="text"><br>'''
+            output += '''Course:<input name="course" type="text"><br>'''
+            output += '''<input type="submit" value="Submit"> </form>'''
+            return output
         if request.method == 'POST':
-            description = request.form['description']
-            price = request.form['price']
-            course = request.form['course']
             menuItem = MenuItem()
             menuItem.name = request.form['name']
             menuItem.description = request.form['description']
@@ -59,19 +43,36 @@ def recordMenuItem(restaurant_id):
             menuItem.restaurant_id = restaurant_id
             session.add(menuItem)
             session.commit()
-            output = ""
-            output = "<h3>Stored: {0}<h3>".format(request.form['name'])
-            return output
+            return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     except Exception as inst:
         print(type(inst))
         print(inst.args)
         print(inst)
 
 
+
 # Task 2: Create route for editMenuItem function here
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit',methods=['GET','POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+    try:
+        if request.method == 'GET':
+            restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+            item = session.query(MenuItem).filter_by(id=menu_id).one()
+            return render_template('editmenuitem.html', restaurant=restaurant, item=item)
+        if request.method == 'POST':
+            menuItem = session.query(MenuItem).filter_by(id=menu_id).one()
+            menuItem.name = request.form['name']
+            menuItem.description = request.form['description']
+            menuItem.course = request.form['course']
+            menuItem.price = request.form['price']
+            session.add(menuItem)
+            session.commit()
+            return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    except Exception as inst:
+        print(type(inst))
+        print(inst.args)
+        print(inst)
+
 
 # Task 3: Create a route for deleteMenuItem function here
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
