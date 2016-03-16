70c1dfec03f246cfbeaece6a292a890fc83a32e6
Wilbert Sequeira <wilbertcr@gmail.com>
2016-03-16 14:34:23 -0700

    Cleaning up a bit.

--- vagrant/restaurant_menu/build_and_run.sh
+++ vagrant/restaurant_menu/build_and_run.sh
@@ -5,25 +5,25 @@ echo "Installing Node.js"
 sudo apt-get install node
 echo "Installing npm package manager"
 sudo apt-get install npm
-echo "Installing JS dependencies."
+echo "Installing NodeJS dependencies and build tools for JS."
+#Change directory to top level of React App.
 cd src/frontend/
+#Install all app dependencies.
 #--no-bin-links tells npm not to create any symbolic links, as npm breaks in vagrant when they're used.
 npm install --save-dev --no-bin-links
-sleep 3
 echo "Building React.js project"
+#Enter tools folder and run gulp from there, that'll pick up the gulpfile.js in there and build the React.js app.
 cd tools
 nodejs ../node_modules/gulp/bin/gulp.js
+#Change directory back to main folder
 cd ../../../
 echo "Setting up PSQL password and Database restaurant"
-ls
+#Run script to setup DB.
 cd src/backend/scripts/
 ./setup_db.sh
-sleep 1s
 cd ../../../
 echo "Setting up SQLAlchemy objects"
-sleep 1s
 python src/backend/python/config/sql_alchemy_setup.py
-sleep 1s
 echo "Installing PyBuilder"
 sudo -H pip install pybuilder
 echo "Installing mockito"
@@ -36,5 +36,4 @@ echo "Installing coverage"
 sudo pip install coverage
 echo "Executing PyBuilder"
 pyb install_dependencies publish
-sleep 3s
 python target/dist/*/server.py
