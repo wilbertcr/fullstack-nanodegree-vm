df4a1b9760e2b5a3da4517d316ed4d2379935143
Wilbert Sequeira <wilbertcr@gmail.com>
2016-04-02 01:26:15 -0700

    Committing:
    - Setup and created automated documenation for React components. Explored several avenues and ultimately settled for gulp-esdocs.
    - Started using JSDoc to document React Components.

--- vagrant/catalog/src/frontend/tools/gulpfile.js
+++ vagrant/catalog/src/frontend/tools/gulpfile.js
@@ -3,9 +3,10 @@ var gulp = require('gulp'),
     watch_semtic = require('../semantic/tasks/watch');
 var babel = require('gulp-babel');
 var webpack_stream = require('webpack-stream');
+var esdoc = require('gulp-esdoc');
 
 gulp.task('transpile',function() {
-        return gulp.src('../components/*.jsx')
+        return gulp.src('../components/*.js')
             .pipe(babel({
                 presets: ['es2016','react'],
                 plugins: ['transform-object-rest-spread']
@@ -14,8 +15,19 @@ gulp.task('transpile',function() {
     }
 );
 
-gulp.task('default',['transpile'], function() {
-    return gulp.src('../compiled-components/index.js')
+gulp.task('doc',['transpile'],function(cb){
+    return gulp.src('../components/')
+        .pipe(esdoc({
+                "destination":"../docs",
+                "plugins":[
+                    {"name": "esdoc-es7-plugin"}
+                ],
+            })
+        );
+});
+
+gulp.task('default',['doc'], function() {
+    return gulp.src('../components/index.js')
         .pipe(webpack_stream(require('./webpack.config.js')))
         .pipe(gulp.dest('../../backend/python/static/javascript/'));
 });
@@ -25,6 +37,9 @@ gulp.task('build_semantic', build_semtic);
 gulp.task('watch_semantic', watch_semtic);
 
 gulp.task('watch_react',function(){
-    gulp.watch('../components/*.jsx',['transpile','default']);
+    gulp.watch('../components/*.js',['default']);
 });
 
+gulp.task('watch_docs',function(){
+    gulp.watch('../components/*.js',['doc']);
+});
