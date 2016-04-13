76d556cfaabf11222c719383dc2f946e720d7951
Wilbert Sequeira <wilbertcr@gmail.com>
2016-04-13 13:16:13 -0700

    Fixed and reorganized build script. It is now subdivided into the frontend and backend parts.
    I also read up some more on PyBuilder and fixed a few things in the script. I am now using virtualenv(recommended by PyBuilder community) after figuring out
    how to make it work in a vagrant shared folder.
    
    PyBuilder still complains that it cannot find a file but everything works so this can be ignored.

---
+++ vagrant/catalog/backendbuild.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -x
+echo "Installing virtual envirionment"
+sudo -H pip install virtualenv
+echo "Creating a virtual envirionment for the project's dependencies."
+virtualenv venv --always-copy
+source venv/bin/activate
+# Run script to setup DB.
+sudo apt-get --assume-yes install libpq-dev python-dev
+sudo apt-get --assume-yes install python-psycopg2
+sudo apt-get --assume-yes install python-flask python-sqlalchemy
+echo "Installing flask"
+sudo pip install flask
+echo "Installing coverage"
+sudo pip install coverage
+echo "Installing PyBuilder"
+sudo -H pip install pybuilder
+echo "Installing mockito"
+sudo pip install mockito
+echo "Installing flake8"
+sudo pip install flake8
+echo "Installing xmlrunner"
+sudo pip install xmlrunner
+echo "Installing coverage"
+sudo pip install coverage
+echo "Installing bleach"
+pip install bleach
+echo "Installing oauth2client"
+pip install oauth2client
+echo "Installing requests"
+pip install requests
+echo "Installing httplib2"
+pip install httplib2
+echo "Installing redis"
+pip install redis
+echo "Installing passlib"
+pip install passlib
+echo "Installing itsdangerous"
+pip install itsdangerous
+echo "Installing psycopg2"
+pip install psycopg2
+echo "Installing flask-httpauth"
+pip install flask-httpauth
+echo "Install PyBuilder"
+pip install pybuilder
+echo "Installing SQLAlchemy"
+pip install SQLAlchemy
+cd src/backend/scripts/
+ls -l
+./setup_db.sh
+cd ../../../
+ls -l
+echo "Executing PyBuilder"
+pyb install_dependencies
+pyb analyze
+pyb
+echo "Moving environment to target directory"
+virtualenv --relocatable venv
+mv venv target/dist/
+cd target/dist/
+ls -l
+./server.py
---
+++ vagrant/catalog/frontendbuild.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -x
+
+echo "Installing Node.js"
+sudo apt-get --assume-yes install node
+echo "Installing npm package manager"
+sudo apt-get --assume-yes install npm
+echo "Installing NodeJS dependencies and build tools for JS."
+# Change directory to top level of React App.
+cd src/frontend/
+ls -l
+# Install all app dependencies.
+# --no-bin-links tells npm not to
+# create any symbolic links, as npm
+# breaks in vagrant when they're used.
+npm install --save-dev --no-bin-links
+echo "Installing gulp globally so that
+command becomes available."
+sudo -H npm install -g gulp
+echo "Building React.js project"
+# This is needed for semantic, it doesn't
+# like that nodejs is not named node
+# installing it fixes that.
+installnodejslegacy="sudo apt-get --assume-yes install nodejs-legacy"
+eval $installnodejslegacy
+#Enter tools folder and run gulp from there,
+# that'll pick up the gulpfile.js in there and
+# run its default task
+cd tools
+ls -l
+node ../node_modules/gulp/bin/gulp
+#Change directory back to main folder
+cd ../../
+ls -l
+cd backend/python/static/
+ls -l
+npm install --save semantic-ui --no-bin-links
+# Newer gulp-autoprefixer breaks building of css.
+# We need to delete the newer one.
+rm -rf node_modules/gulp-autoprefixer
+# And install the older version that does work.
+npm install gulp-autoprefixer@2.3.1 --no-bin-links
+cd semantic
+gulp build
+cd ../../../
+ls -l
