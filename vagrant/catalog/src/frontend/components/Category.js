674504d7dc7ded1921dc6a2a309bbdab3bff443f
Wilbert Sequeira <wilbertcr@gmail.com>
2016-04-14 09:12:45 -0700

    Added DeleteCategoryForm and its respective modal.

--- vagrant/catalog/src/frontend/components/Category.js
+++ vagrant/catalog/src/frontend/components/Category.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Component from './Component';
 import ModalPageGeneric from './ModalPageGeneric'
 import EditCategoryForm from './EditCategoryForm'
+import DeleteCategoryForm from './DeleteCategoryForm'
 
 /**
  * Represents the modal itself.
@@ -15,14 +16,12 @@ export default class Category extends Component{
     constructor(props) {
         super(props);
         this.state = {name:'',
-            isModalVisible: false,
+            isEditModalVisible: false,
+            isDeleteModalVisible: false,
             active: false,
         };
-        this.reactComponent = <element></element>;
-    }
-
-    deleteCategory(){
-        this.props.deleteCategory(this.props.category.id);
+        this.EditReactElement = <element></element>;
+        this.DeleteReactElement = <element></element>;
     }
 
     displayCategory(){
@@ -35,9 +34,14 @@ export default class Category extends Component{
     /**
      *
      * */
-    switchModalVisibility(){
-        console.log("Modal is now:"+this.state.isModalVisible ? 'visible': 'hidden');
-        this.setState({...this.state,isModalVisible: !this.state.isModalVisible});
+    switchEditModalVisibility(){
+        console.log("Modal is now:"+this.state.isEditModalVisible ? 'visible': 'hidden');
+        this.setState({...this.state,isEditModalVisible: !this.state.isEditModalVisible});
+    }
+
+    switchDeleteModalVisibility(){
+        console.log("Modal is now:"+this.state.isDeleteModalVisible ? 'visible': 'hidden');
+        this.setState({...this.state,isDeleteModalVisible: !this.state.isDeleteModalVisible});
     }
 
     setActiveCategory(){
@@ -49,9 +53,14 @@ export default class Category extends Component{
     }
 
     render(){
-        this.reactElement = <EditCategoryForm
+        console.log("working");
+        this.EditReactElement = <EditCategoryForm
             {...this.props}
-            switchModalVisibility={this.switchModalVisibility}
+            switchModalVisibility={this.switchEditModalVisibility}
+        />;
+        this.DeleteReactElement = <DeleteCategoryForm
+            {...this.props}
+            switchModalVisibility={this.switchDeleteModalVisibility}
         />;
         var classes = this.props.activeCategory==this.props.category.id ?
             "blue left active item":
@@ -60,11 +69,11 @@ export default class Category extends Component{
             <a className={classes}>
                 <i className="trash red icon"
                    style={{display: this.props.loginStatus.value ? 'inline-block' : 'none'}}
-                   onClick={this.deleteCategory}>
+                   onClick={this.switchDeleteModalVisibility}>
                 </i>
                 <i className="edit blue icon"
                       style={{display: this.props.loginStatus.value ? 'inline-block' : 'none'}}
-                      onClick={this.switchModalVisibility}>
+                      onClick={this.switchEditModalVisibility}>
                 </i>
                 <div className="ui blue circular label"
                      onClick={this.displayCategory}>
@@ -75,10 +84,16 @@ export default class Category extends Component{
                     {this.props.category.name}
                 </div>
                 <ModalPageGeneric category={this.props.category}
-                                  isVisible={this.state.isModalVisible}
-                                  switchModalVisibility={this.switchModalVisibility}
+                                  isVisible={this.state.isEditModalVisible}
+                                  switchModalVisibility={this.switchEditModalVisibility}
                                   editCategory={this.props.editCategory}
-                                  reactComponent={this.reactElement}
+                                  reactComponent={this.EditReactElement}
+                />
+                <ModalPageGeneric category={this.props.category}
+                                  isVisible={this.state.isDeleteModalVisible}
+                                  switchModalVisibility={this.switchDeleteModalVisibility}
+                                  deleteCategory={this.props.deleteCategory}
+                                  reactComponent={this.DeleteReactElement}
                 />
            </a>
         );
